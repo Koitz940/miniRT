ad0451c3b855088237108a8438847cfa6764db7f
gcassi-d <gcassi-d@student.42urduliz.com>
2026-03-03 19:30:23 +0100

    small

--- srcs/main_loop/intersect.c
+++ srcs/main_loop/intersect.c
@@ -6,7 +6,7 @@
 /*   By: gcassi-d <gcassi-d@42urduliz.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/21 17:52:12 by gcassi-d          #+#    #+#             */
-/*   Updated: 2026/03/01 14:33:16 by gcassi-d         ###   ########.fr       */
+/*   Updated: 2026/03/03 19:28:44 by gcassi-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,20 @@ int	intersect_plane(t_plane *plane,
 		t_vec dir, t_pixel *pixel, t_camera *camera)
 {
 	double	denom;
+	double	numer;
 	double	t;
 
 	denom = dot_prod(plane->dir, dir);
-	if (denom == 0.0)
-		return (0);
-	t = -dot_prod(plane->dir, points_vec(camera->pos, plane->pos)) / denom;
+	numer = dot_prod(plane->dir, points_vec(camera->pos, plane->pos));
+	if (denom != 0.0)
+		t = numer / denom;
+	else
+	{
+		if (numer == 0)
+			t = 2 * TOL;
+		else
+			return (0);
+	}
 	if (t < pixel->t && t > TOL)
 	{
 		pixel->t = t;
@@ -44,10 +52,10 @@ int	intersect_sphere(t_sphere *sphere, t_vec dir,
 				points_vec(camera->pos,
 					sphere->pos)), 2) - sphere->d * sphere->d;
 	dis = m * m - c;
-	if (dis < 0)
+	if (dis < 0.0)
 		return (0);
 	dis = sqrt(dis);
-	if (m - dis >= TOL)
+	if (m - dis > TOL)
 		t = m - dis;
 	else
 		t = m + dis;
@@ -63,21 +71,5 @@ int	intersect_sphere(t_sphere *sphere, t_vec dir,
 int	intersect_cylinder(t_cylinder *cylinder, t_vec dir,
 		t_pixel *pixel, t_camera *camera)
 {
-	t_vec	aux;
-	double	height;
-	double	r;
-
-	aux.x = pos.x - cylinder->pos.x;
-	aux.y = pos.y - cylinder->pos.y;
-	aux.z = pos.z - cylinder->pos.z;
-	height = dot_prod(aux, cylinder->dir);
-	if (cylinder->h / 2.0 - fasb(height) < -TOL)
-		return (0);
-	move_by(&(aux), cylinder->dir, -height);
-	r = sqrt(mod(aux.x, aux.y, aux.z) - cylinder->d);
-	if (r < TOL && fabs(cylinder->h / 2.0 - fasb(height)) < TOL)
-		return (1);
-	else if (fabs(r) < TOL)
-		return (1);
-	return (0);
+	
 }
